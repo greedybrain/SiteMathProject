71b9db4bd8fdf7413ef338e2cbbb39c44a0778be
EducationPurposesOnly <educationpurposesonly@Nayas-MacBook-Pro.local>
2020-05-28 09:27:54 -0400

    added heroku link to cors origins

--- site_math_api/config/initializers/cors.rb
+++ site_math_api/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'https://greedybrain.github.io/SiteMathProject/site_math_client/'
 
     resource '*',
       headers: :any,
